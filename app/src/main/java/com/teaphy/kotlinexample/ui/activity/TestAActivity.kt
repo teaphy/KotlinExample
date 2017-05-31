d3ab2a8f7ee1b10e951e37a3958054f85bf05440
todo <334679056@qq.com>
2017-05-31 20:43:42 +0800

    1.gradle插 2.20170523

--- app/src/main/java/com/teaphy/kotlinexample/ui/activity/TestAActivity.kt
+++ app/src/main/java/com/teaphy/kotlinexample/ui/activity/TestAActivity.kt
@@ -2,45 +2,29 @@ package com.teaphy.kotlinexample.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.teaphy.kotlinexample.R
-import com.teaphy.kotlinexample.ui.Base.BaseActivity
+import android.os.PersistableBundle
+import android.view.Gravity
+import org.jetbrains.anko.textView
 
-import kotlinx.android.synthetic.main.activity_test_a.*
-import org.jetbrains.anko.dip
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 /**
  * @desc  测试 启动新的Activity 调用
  * @author Teaphy
  * @date 2017/5/25
  */
-class TestAActivity : BaseActivity() {
-
+class TestAActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+       verticalLayout{
+           val name = textView("王者荣耀")
+           button("sumbit"){
+               onClick { toast(name.text) }
+           }
+       }
     }
-
-    override fun getLayoutId(): Int {
-        return R.layout.activity_test_a
-    }
-
-    override fun initTitle(): CharSequence? {
-        return "TestA"
-    }
-
-    override fun initData() {
-
-    }
-
-    override fun initView() {
-        val id = intent.getIntExtra("id", -1)
-        val name = intent.getStringExtra("name")
-
-        tvA.text = "id: $id | name=$name"
-    }
-
-    override fun setListener() {
-    }
-
-
 }
+
+
